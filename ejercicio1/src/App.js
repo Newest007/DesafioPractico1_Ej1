9cf8c42629ebefdfde5b1743b489e14401a5bbc0
Ander503 <andersonmelendez73@gmail.com>
2023-09-01 07:41:06 -0600

    Componentes Input

--- ejercicio1/src/App.js
+++ ejercicio1/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
+import Input from './components/Input';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <p>Ingrese su nombre</p>
+     <Input />
+     <p>Ingrese su Apellido</p>
+     <Input />
+     <p>Ingrese su nombre</p>
+     <Input />
     </div>
   );
 }
---
+++ ejercicio1/src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Input(props){
+    return(
+        <div>
+            <input type='text' name=''></input>
+        </div>
+    );
+}
+
+export default Input;
